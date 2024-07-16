5c0df4139964ea9b3a9b8edd678d2abee28b830e
Your Name <you@example.com>
2024-07-16 07:20:25 +0530

    Product Service

---
+++ src/main/java/com/scaler/productservicejune2024/Productservicejune2024Application.java
@@ -0,0 +1,13 @@
+package com.scaler.productservicejune2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Productservicejune2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Productservicejune2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/scaler/productservicejune2024/Productservicejune2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.productservicejune2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Productservicejune2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
